5525a50f8ed2626c85af2616d0db22976fd77ae2
DaniilGoryunov <dgoryunov160604@gmail.com>
2023-09-23 11:19:17 +0300

    first lab w/o gtests

---
+++ lab01/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+
+
+
+int main() {
+    std::string filename;
+    std::cin >> filename;
+    int pipe1_fd[2];
+    pipe(pipe1_fd);
+    int id = fork();
+    if (id < 0){
+        perror("fork failed");
+        return 1;
+    }
+    else if(id > 0){
+        close(pipe1_fd[0]);
+
+        float num;
+        while (std::cin >> num) {
+            write(pipe1_fd[1], &num, sizeof(num));
+        }
+
+        close(pipe1_fd[1]);
+    }
+    else{
+        close(pipe1_fd[1]);
+
+        if (dup2(pipe1_fd[0], STDIN_FILENO) == -1) {
+            perror("dup2 error");
+            return 1;
+        }
+
+        execl("./child", "./child", filename.c_str(), NULL);
+
+        close(pipe1_fd[0]);
+    }
+    
+}
